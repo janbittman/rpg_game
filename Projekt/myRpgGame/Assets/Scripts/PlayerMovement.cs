97c68a643f6cc54ef237664add10883d0881ad11
janbittman <59982464+janbittman@users.noreply.github.com>
2020-01-19 00:14:18 +0100

    Added movement animations

--- Projekt/myRpgGame/Assets/Scripts/PlayerMovement.cs
+++ Projekt/myRpgGame/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidBody2D;
     private Vector3 change;
-
+    private Animator myAnimator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidBody2D = GetComponent<Rigidbody2D>();  
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -24,9 +25,22 @@ public class PlayerMovement : MonoBehaviour
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
         Debug.Log(change);
+        updateAnimationAndMove();
+
+    }
+
+    void updateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            myAnimator.SetFloat("moveX", change.x);
+            myAnimator.SetFloat("moveY", change.y);
+            myAnimator.SetBool("moving", true);  
+        }
+        else
+        {
+            myAnimator.SetBool("moving", false);
         }
     }
     void MoveCharacter()
