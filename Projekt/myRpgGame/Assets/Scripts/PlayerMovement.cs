c749cc2b601a5e59dc28b42968f8d55439c1fbf4
Jan Bittman <jan.bittman2@gmail.com>
2020-01-18 18:32:30 +0100

    added player movement

---
+++ Projekt/myRpgGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody2D;
+    private Vector3 change;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(change);
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+    void MoveCharacter()
+    {
+        myRigidBody2D.MovePosition(transform.position + change.normalized * speed * Time.deltaTime);
+    }
+
+
+}
